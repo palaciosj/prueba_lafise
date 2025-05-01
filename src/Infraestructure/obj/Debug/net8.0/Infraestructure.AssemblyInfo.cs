cc92b60c0b5001d07fe55864e5650f00f1490c1a
palaciosj <palaciosj1551@gmail.com>
2025-04-30 22:34:35 -0600

    feat: implement global error handling for validation and not found exceptions

--- src/Infraestructure/obj/Debug/net8.0/Infraestructure.AssemblyInfo.cs
+++ src/Infraestructure/obj/Debug/net8.0/Infraestructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+436dff14623ac86c6131d977593adc8479320506")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36862845d52ede683758787880b72801741b987d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,6 +1,7 @@
 using Web.Endpoints;
 using MediatR;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +19,45 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseExceptionHandler(config =>
+{
+    config.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+        if (exception is FluentValidation.ValidationException validationEx)
+        {
+            var errors = validationEx.Errors
+                .GroupBy(e => e.PropertyName)
+                .ToDictionary(
+                    g => g.Key,
+                    g => g.Select(e => e.ErrorMessage).ToArray()
+                );
+
+            context.Response.StatusCode = 400;
+            context.Response.ContentType = "application/json";
+
+            var response = new
+            {
+                message = "Validation failed",
+                errors
+            };
+
+            await context.Response.WriteAsJsonAsync(response);
+        }
+        else if (exception is KeyNotFoundException)
+        {
+            context.Response.StatusCode = 404;
+            var response = new
+            {
+                message = exception.Message
+            };
+            await context.Response.WriteAsJsonAsync(response);
+        }
+        
+    });
+});
 
 app.UseHttpsRedirection();
 CustomerEndpoints.Map(app);
